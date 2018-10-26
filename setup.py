b7b28b799032cfeb932de284e95e1aa4be8b6d7d
Erick Salas <ericksc@gmail.com>
2018-10-26 02:34:06 -0600

    mi primera version de ejemplo tox

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PythonBasico_102018',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Erix',
+    author_email='',
+    description=''
+)
